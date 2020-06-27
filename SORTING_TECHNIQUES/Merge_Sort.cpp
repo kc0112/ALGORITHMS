19e2792b7d85e5bd5b34ccc035f30f1dbe8992a9
Khushi Chavada <khushichavada0112@gmail.com>
2020-06-27 12:38:34 +0530

    Update Merge_Sort.cpp

--- SORTING_TECHNIQUES/Merge_Sort.cpp
+++ SORTING_TECHNIQUES/Merge_Sort.cpp
@@ -1,58 +1,65 @@
 #include<iostream>
 using namespace std;
+void merge_sort(int a,int n);
+void merg(int a[],int b[],int lb,int mid,int ub);
+void mergesort(int a[],int temp[],int lb,int ub);
 
-void merg(int a[],int lb,int mid,int ub)
+void merg(int a[],int b[],int lb,int mid,int ub)
 {
-     int i=lb,j=mid+1,k=lb,b[10];
+     int i=lb,j=mid+1,k=lb;
      while(i<=mid&&j<=ub)
      {
          if(a[i]<=a[j])
          {
-             b[k]=a[i];
-             i++;k++;
+             b[k++]=a[i++];
          }
          else
          {
-             b[k]=a[j];
-             j++;k++;
-         }
+             b[k++]=a[j++];
+        }
      }
      if(i>mid)
      {
          while(j<=ub)
          {
-             b[k]=a[j];
-             j++;k++;
+             b[k++]=a[j++];
          }
      }
      else if(j>ub)
      {
          while(i<=mid)
          {
-             b[k]=a[i];
-             i++;k++;
+             b[k++]=a[i++];
          }
      }
  for(k=lb;k<=ub;k++)
    a[k]=b[k];
 }
 
-void mergesort(int a[],int lb,int ub)
+
+void mergesort(int a[],int temp[],int lb,int ub)
 {
     if(lb<ub)
     {
         int mid=(lb+ub)/2;
-        mergesort(a,lb,mid);
-        mergesort(a,mid+1,ub);
-        merg(a,lb,mid,ub);
+        mergesort(a,temp,lb,mid);
+        mergesort(a,temp,mid+1,ub);
+        merg(a,temp,lb,mid,ub);
     }
 
+}
+
+void merge_sort(int a[],int n)
+{
+    int temp[n];
+    mergesort(a,temp,0,n-1);
 }
 
 int main()
 {
      int a[]{15,5,24,8,1,3,16,10,20},i;
-     mergesort(a,0,8);
+     int n=sizeof(a)/sizeof(a[0]);
+     merge_sort(a,n);
      for(i=0;i<9;i++)
         cout<<a[i]<<" ";
 
