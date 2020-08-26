ba65f7f5694189c8dbea70f627732e1fb1a95bd2
Khushi Chavada <khushichavada0112@gmail.com>
2020-08-26 10:53:59 +0530

    Add files via upload

---
+++ GREEDY/Activity_selection_problem.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> start =  {1, 3, 0, 5, 8, 5};
+    vector<int> finish =  {2, 4, 6, 7, 9, 9};
+
+    map<int,int>prob;
+    for(int i{0};i<start.size();i++){
+        prob.insert(make_pair(finish[i],start[i]));
+    }
+    auto it =prob.begin();
+    cout<<it->second<<":"<<it->first<<endl;
+    it++;
+    auto prev=prob.begin();
+    while(it!=prob.end()){
+        if(prev->first<=it->second){
+            cout<<it->second<<":"<<it->first<<endl;
+            prev=it;
+        }
+        it++;
+    }
+}
