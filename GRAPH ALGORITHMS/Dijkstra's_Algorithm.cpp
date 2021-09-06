1c04670fc86d0c51615640f4b6276ee3cc20c1d2
Khushi Chavada <khushichavada0112@gmail.com>
2021-09-06 14:03:51 +0530

    Update Dijkstra's_Algorithm.cpp

--- GRAPH ALGORITHMS/Dijkstra's_Algorithm.cpp
+++ GRAPH ALGORITHMS/Dijkstra's_Algorithm.cpp
@@ -1,96 +1,30 @@
-// Program to find Dijkstra's shortest path using
-// priority_queue in STL
-#include<bits/stdc++.h>
-using namespace std;
-# define INF 0x3f3f3f3f
-
-// iPair ==> Integer Pair
-typedef pair<int, int> iPair;
-
-// To add an edge
-void addEdge(vector <pair<int, int> > adj[], int u,
-                                     int v, int wt)
-{
-    adj[u].push_back(make_pair(v, wt));
-    adj[v].push_back(make_pair(u, wt));
-}
-
-
-// Prints shortest paths from src to all other vertices
-void shortestPath(vector<pair<int,int> > adj[], int V, int src)
-{
-    // Create a priority queue to store vertices that
-    // are being preprocessed. 
-    priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
-
-    // Create a vector for distances and initialize all
-    // distances as infinite (INF)
-    vector<int> dist(V, INF);
-    vector<int>parent(V,src);
-    // Insert source itself in priority queue and initialize
-    // its distance as 0.
-    pq.push(make_pair(0, src));
-    dist[src] = 0;
-
-    /* Looping till priority queue becomes empty (or all
-    distances are not finalized) */
-    while (!pq.empty())
-    {
-        // The first vertex in pair is the minimum distance
-        // vertex, extract it from priority queue.
-        // vertex label is stored in second of pair (it
-        // has to be done this way to keep the vertices
-        // sorted distance (distance must be first item
-        // in pair)
-        int u = pq.top().second;
-        pq.pop();
-        // Get all adjacent of u.
-        for (auto x : adj[u])
-        {
-            // Get vertex label and weight of current adjacent
-            // of u.
-            int v = x.first;
-            int weight = x.second;
-
-            // If there is shorted path to v through u.
-            if ((dist[v] > dist[u] + weight))
-            {
-                // Updating distance of v
-                dist[v] = dist[u] + weight;
-                parent[v]=u;
-                pq.push(make_pair(dist[v], v));
+ vector <int> dijkstra(int V, vector<vector<int>> adj[], int s){
+        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        
+        pq.push(make_pair(0,s));
+        
+        vector<int>visited(V,-1);
+    
+        while(!pq.empty()){
+            int node = pq.top().second;
+            int wsf = pq.top().first;
+            pq.pop();
+            
+            if(visited[node]!=-1){
+                continue;
+            }
+            visited[node] = wsf;
+            
+            vector<vector<int>>edges = adj[node];
+            
+            for(vector<int>edge:edges){
+                int nbr = edge[0]; int wt = edge[1];
+                
+                if(visited[nbr] == -1)
+                    pq.push(make_pair(wt+wsf,nbr));
             }
         }
-    }
-
-    // Print shortest distances stored in dist[]
-    cout<<"Vertex Distance from Source\n";
-    for (int i = 0; i < V; ++i)
-        cout<<i<<" \t\t "<<dist[i]<<" \t\t "<<parent[i]<<"\n";
-}
-
-// Driver program to test methods of graph class
-int main()
-{
-    int V = 9;
-    vector<iPair > adj[V];
-
-    // making above shown graph
-    addEdge(adj, 0, 1, 4);
-    addEdge(adj, 0, 7, 8);
-    addEdge(adj, 1, 2, 8);
-    addEdge(adj, 1, 7, 11);
-    addEdge(adj, 2, 3, 7);
-    addEdge(adj, 2, 8, 2);
-    addEdge(adj, 2, 5, 4);
-    addEdge(adj, 3, 4, 9);
-    addEdge(adj, 3, 5, 14);
-    addEdge(adj, 4, 5, 10);
-    addEdge(adj, 5, 6, 2);
-    addEdge(adj, 6, 7, 1);
-    addEdge(adj, 6, 8, 6);
-    addEdge(adj, 7, 8, 7);
-
-    shortestPath(adj, V, 0);
-    return 0;
-}
+        
+        
+        return visited;
+ }
